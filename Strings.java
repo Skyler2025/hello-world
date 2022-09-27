eed9cd0b2ea0d7ef1c36b15eca1f169c8c5b45bf
Skyler Nguyen <skyler.nguyen@mrjaffesclass.com>
2022-09-27 14:02:56 -0700

    Added Strings Project

---
+++ Strings.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Strings
+{
+    public static void main(String[] args) {
+    String string1 = "I am a string";
+    String string2 = "I am a string";
+    String string3 = "i am also a string";
+    boolean stringsEqual = string1.equals(string3);
+    System.out.println(string1);
+    System.out.println(string2);
+    System.out.println(string3);
+    System.out.println(stringsEqual);
+    }
+}
